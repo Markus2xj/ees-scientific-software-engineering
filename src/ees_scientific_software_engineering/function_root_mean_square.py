4dba13b2e34ab6468464af49b867a64360cd257b
Markus2xj <j.j.markus@tue.nl>
2024-12-17 10:54:04 +0100

    revised docstring

--- src/ees_scientific_software_engineering/function_root_mean_square.py
+++ src/ees_scientific_software_engineering/function_root_mean_square.py
@@ -3,6 +3,11 @@ import numpy as np
 
 def rms(input_array: np.ndarray) -> float:
     """
-    This function calculates the rms based on np functions.
+    This function calculates the rms based on np
+    Parameters:
+    np.ndarray: input_array
+
+    Returns:
+    float: rms value
     """
     return np.sqrt(np.mean(input_array**2))
