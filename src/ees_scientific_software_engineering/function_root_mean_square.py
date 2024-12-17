a14c85e644acdccf62821377e1fd34ddddeb6d23
Markus2xj <j.j.markus@tue.nl>
2024-12-17 11:46:20 +0100

    First 3 Errors init commit

--- src/ees_scientific_software_engineering/function_root_mean_square.py
+++ src/ees_scientific_software_engineering/function_root_mean_square.py
@@ -4,8 +4,13 @@ This is a docstring
 
 import numpy as np
 
+class DimensionError(Exception):
+    """
+    Custom exception for dimensionality error
+    """
+    pass
 
-def rms(input_array: np.ndarray) -> float:
+def rms(input_array: np.ndarray) -> float:  # input_array should be an instance of np.ndarray.
     """
     This function calculates the rms based on np
     Parameters:
@@ -14,4 +19,13 @@ def rms(input_array: np.ndarray) -> float:
     Returns:
     float: rms value
     """
+    if not isinstance(input_array,np.ndarray):
+        raise TypeError("Type is not np.ndarray!")
+
+    if input_array.ndim != 1:
+        raise DimensionError("Dimension of inputarray is not one")
+
+    if input_array.dtype != np.float64:
+        raise TypeError("Datatype is not np.float64")
+
     return np.sqrt(np.mean(input_array**2))
--- tests/test_function_root_mean_square.py
+++ tests/test_function_root_mean_square.py
@@ -1,8 +1,24 @@
 import numpy as np
+import pytest
 
-from ees_scientific_software_engineering.function_root_mean_square import rms
+from ees_scientific_software_engineering.function_root_mean_square import rms,DimensionError
 
 
 def test_rms():
     a = np.ones(10)
     assert rms(a) == 1
+
+def testInputType():
+    a = [1,2,3,4,5]
+    with pytest.raises(TypeError, match="Type is not np.ndarray!"):
+        rms(a)
+
+def testDimensionality():
+    a = np.ones((10,10))
+    with pytest.raises(DimensionError, match="Dimension of inputarray is not one"):
+        rms(a)
+
+def testDType():
+    a = np.ones(10,dtype='int')
+    with pytest.raises(TypeError, match="Datatype is not np.float64"):
+        rms(a)
