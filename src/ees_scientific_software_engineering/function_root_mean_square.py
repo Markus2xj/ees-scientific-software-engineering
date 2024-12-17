b8ab633eff8f5056bd0955f9ceff1881c43dfe99
XinLiTue <x.li7@tue.nl>
2024-12-17 12:32:59 +0100

    after isort+black

--- src/ees_scientific_software_engineering/function_root_mean_square.py
+++ src/ees_scientific_software_engineering/function_root_mean_square.py
@@ -4,15 +4,20 @@ This is a docstring
 
 import numpy as np
 
+
 class SizeError(Exception):
     """
     Custom expection size for less than one
     """
+
     pass
+
+
 class NanError(Exception):
     """
     Custom expection contains nan
     """
+
     pass
 
 
@@ -28,9 +33,9 @@ def rms(input_array: np.ndarray) -> float:
     if np.size(input_array) < 1:
         raise SizeError("The size of the input array should be at least one!")
 
-    if np.any(np.isinf(input_array)) and not np.any(np.isnan(input_array)) :
+    if np.any(np.isinf(input_array)) and not np.any(np.isnan(input_array)):
         raise ValueError("There should be no inf in the input array!")
-    
+
     if np.any(np.isnan(input_array)) and not np.any(np.isinf(input_array)):
         raise NanError("There should be no nan in the input array!")
 
--- tests/test_function_root_mean_square.py
+++ tests/test_function_root_mean_square.py
@@ -1,25 +1,29 @@
 import numpy as np
 import pytest
-from ees_scientific_software_engineering.function_root_mean_square import rms, SizeError, NanError
+
+from ees_scientific_software_engineering.function_root_mean_square import NanError, SizeError, rms
 
 
 def test_rms():
     a = np.ones(10)
     assert rms(a) == 1
 
+
 def test_size_error():
     a = np.ones(0)
     with pytest.raises(SizeError, match="The size of the input array should be at least one!"):
         rms(a)
 
+
 def test_inf_error():
     a = np.ones(10)
-    a[0]= np.inf
+    a[0] = np.inf
     with pytest.raises(ValueError, match="There should be no inf in the input array!"):
         rms(a)
 
+
 def test_nan_error():
     a = np.ones(10)
-    a[0]= np.nan
+    a[0] = np.nan
     with pytest.raises(NanError, match="There should be no nan in the input array!"):
         rms(a)
